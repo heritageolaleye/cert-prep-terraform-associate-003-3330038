8f98608587dee6017df4fff85ae2cbaf0da00163
heritageolaleye <heritageolaleye@gmail.com>
2025-01-31 12:10:53 +0100

    main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
-resource "aws_instance" "demo-1" {
+resource "aws_instance" "ec2-instance" {
     #instance configuration
+    ami = "ami-08d4f6bbae664bd41"
+    instance_type = "t2.micro"
+
+    tags = {
+      Name = "demo-1"
+    }
 }
