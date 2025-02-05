e7ca19d11c04f24d6ba13619283ed23afe1394cd
heritageolaleye <heritageolaleye@gmail.com>
2025-02-05 17:04:31 +0100

    updated

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = "terraform-demo-s3-bucket1"
+    bucket         = "terraform-demo-s3-bucket10"
     key            = "terraform-state-dir/terraform.tfstate"
     region         = "us-east-1"
     profile        = "Adelafight"
