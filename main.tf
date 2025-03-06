311908bd5ab87d61971eb0aef275e9943d57a738
heritageolaleye <heritageolaleye@gmail.com>
2025-03-06 16:56:44 +0100

    updated

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ data "aws_vpc" "default" {
   default = true
 }
 data "aws_subnet" "default_subnet" {
-  vpc_id = data.aws_vpc.default_subnet
+  vpc_id = data.aws_vpc.default.id
   availability_zone =  "us-east-1a"
 }
 
